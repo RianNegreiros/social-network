640ab147c09627e800ce958a580da89cb56b7de6
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-06 20:06:25 -0300

    get activities from the client

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://localhost:5001/api/activities").then(response => {
+      console.log(response);
+      setActivities(response.data);
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {activities.map((activity: any )=> (
+            <li key={activity.id}>
+              {activity.title}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
