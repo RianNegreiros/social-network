8420947b0a093759e566fd06c1d0b9b4c83608a3
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-06 01:36:03 -0300

    add entity framework

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
