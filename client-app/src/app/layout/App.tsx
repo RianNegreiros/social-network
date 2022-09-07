895a894e78d21326ae03f2db5c5323a4a1d0688e
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-07 01:24:01 -0300

    create activity interface

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { Header, List } from 'semantic-ui-react';
 import './App.css';
 import axios from 'axios';
+import { Activity } from '../models/activity';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get("https://localhost:5001/api/activities").then(response => {
-      console.log(response);
+    axios.get<Activity[]>("https://localhost:5001/api/activities").then(response => {
       setActivities(response.data);
     })
   }, [])
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <Header as='h2' icon='users' content='Activities' />
       <List>
-        {activities.map((activity: any )=> (
+        {activities.map(activity => (
               <List.Item key={activity.id}>
                 {activity.title}
               </List.Item>
