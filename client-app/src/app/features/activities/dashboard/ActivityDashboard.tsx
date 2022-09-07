92e2c8c19a008488dd72871cecec601d9f746d42
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-07 17:29:22 -0300

    handle create and edit

--- client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -13,10 +13,11 @@ interface Props {
   editMode: boolean;
   openForm: (id: string) => void;
   closeForm: () => void;
+  createOrEdit: (activity: Activity) => void;
 }
 
 export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectActivity,
-        editMode, openForm, closeForm}: Props) {
+        editMode, openForm, closeForm, createOrEdit}: Props) {
   return (
     <Grid>
       <Grid.Column width='10'>
@@ -30,7 +31,7 @@ export default function ActivityDashboard({activities, selectedActivity, selectA
         openForm={openForm}
         />}
         {editMode &&
-        <ActivityForm closeForm={closeForm} activity={selectedActivity} />}
+        <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
       </Grid.Column>
     </Grid>
   )
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -33,6 +33,14 @@ function App() {
     setEditMode(false);
   }
 
+  function handleCreateOrEditActivity(activity: Activity) {
+    activity.id
+      ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
+      : setActivities([...activities, activity]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
   return (
     <>
         <NavBar openForm={handleFormOpen} />
@@ -45,6 +53,7 @@ function App() {
           editMode={editMode}
           openForm={handleFormOpen}
           closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditActivity}
           />
         </Container>
     </>
