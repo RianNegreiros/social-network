4b0c02f4c7a5b791ac0d711cd0e90cc170fa5b89
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-08 00:07:25 -0300

    add activity store

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,30 @@
+import { makeAutoObservable } from "mobx";
+import agent from "../api/agent";
+import { Activity } from "../models/activity";
+
+export default class ActivityStore {
+  activities: Activity[] = []
+  selectedActivity: Activity | null = null;
+  editMode = false;
+  loading = false;
+  loadingInitial = false;
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  loadActivities = async () => {
+    this.loadingInitial = true;
+    try {
+      const activities = await agent.Activities.list();
+      activities.forEach(activity => {
+        activity.date = activity.date.split('T')[0];
+        this.activities.push(activity);
+      })
+      this.loadingInitial = false;
+    } catch (error) {
+      console.log(error);
+      this.loadingInitial = false;
+    }
+  }
+}
