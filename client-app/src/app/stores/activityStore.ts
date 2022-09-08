92ec905a0341b5463402185bf623614afff985b2
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-08 00:16:06 -0300

    strict mode

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,4 +1,4 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
 
@@ -14,17 +14,25 @@ export default class ActivityStore {
   }
 
   loadActivities = async () => {
-    this.loadingInitial = true;
+    this.setLoadingInitial(true)
     try {
       const activities = await agent.Activities.list();
-      activities.forEach(activity => {
-        activity.date = activity.date.split('T')[0];
-        this.activities.push(activity);
+      runInAction(() => {
+        activities.forEach(activity => {
+          activity.date = activity.date.split('T')[0];
+          this.activities.push(activity);
+        })
       })
-      this.loadingInitial = false;
+      this.setLoadingInitial(false);
     } catch (error) {
       console.log(error);
-      this.loadingInitial = false;
+      runInAction(() => {
+        this.setLoadingInitial(false);
+      })
     }
+  }
+
+  setLoadingInitial = (state: boolean) => {
+    this.loadingInitial = state;
   }
 }
