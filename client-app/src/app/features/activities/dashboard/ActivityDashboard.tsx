d521a7e30f2890f52eaeed5810e9e41cdd4cf0b6
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-07 21:07:28 -0300

    deleting on the database

--- client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -27,6 +27,7 @@ export default function ActivityDashboard({activities, selectedActivity, selectA
         activities={activities} 
         selectActivity={selectActivity}
         deleteActivity={deleteActivity}
+        submitting={submitting}
         />
       </Grid.Column>
       <Grid.Column width='6'>
