e9e0dc63ecc3254dcfc6cb4c257c7495eedd6699
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-08 00:07:43 -0300

    implement activity store

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
+import { store, StoreContext } from './app/stores/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
+  <StoreContext.Provider value={store}>
     <App />
+  </StoreContext.Provider>
 );
