9cea1cf43c3d1ab74f8b9d2c8c77e22a52ce8ce2
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-09 20:16:42 -0300

    fix gh-13

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -10,7 +10,7 @@ namespace API.DTOs
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$")]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$", ErrorMessage = "Password need at least one number, lowercase and uppercase character, and be beetwen 8 and 16 long.")]
         public string Password { get; set; }
         [Required]
         public string Username { get; set; }
