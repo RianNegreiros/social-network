d95985d76d9ce0c405cbf9b00ae00fbf912fa5da
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-08 23:40:14 -0300

    activity details page

--- client-app/src/app/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityListItem.tsx
@@ -1,54 +1,48 @@
-import React, { SyntheticEvent, useState } from "react";
-import { Button, Icon, Item, ItemContent, Label, Segment } from "semantic-ui-react";
-import { Link } from "react-router-dom";
-import { Activity } from "../../../models/activity";
-import { useStore } from "../../../stores/store";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Icon, Item, Segment } from 'semantic-ui-react';
+import { Activity } from '../../../models/activity';
 
 interface Props {
-  activity: Activity
+    activity: Activity
 }
 
-export default function ActivityListItem({activity}: Props) {
-  const {activityStore} = useStore();
-  const {deleteActivity, loading} = activityStore;
+export default function ActivityListItem({ activity }: Props) {
 
-  const [target, setTarget] = useState('');
-
-  function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-    setTarget(e.currentTarget.name);
-    deleteActivity(id);
-  }
-  
-  return (
-    <Segment.Group>
-      <Segment>
-        <Item.Group>
-          <Item>
-            <Item.Image size="tiny" circular src='/assets/user.png' />
-            <Item.Header as={Link} to={`/activities/${activity.id}`}>{activity.title}</Item.Header>
-            <Item.Description>Hosted by Bob</Item.Description>
-          </Item>
-        </Item.Group>
-      </Segment>
-      <Segment>
-        <span>
-          <Icon name="clock" /> {activity.date}
-          <Icon name="marker" /> {activity.venue}
-        </span>
-      </Segment>
-      <Segment secondary>
-        Attendees go here
-      </Segment>
-      <Segment clearing>
-        <span>{activity.description}</span>
-        <Button
-        as={Link}
-        to={`/activities/${activity.id}`}
-        color='teal'
-        floated="right"
-        content='View'
-        />
-      </Segment>
-    </Segment.Group>
-  )
+    return (
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size='tiny' circular src='/assets/user.png' />
+                        <Item.Content>
+                            <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                                {activity.title}
+                            </Item.Header>
+                            <Item.Description>Hosted by Bob</Item.Description>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' /> {activity.date}
+                    <Icon name='marker' /> {activity.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                Attendees go here
+            </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button 
+                    as={Link}
+                    to={`/activities/${activity.id}`}
+                    color='teal'
+                    floated='right'
+                    content='View'
+                />
+            </Segment>
+        </Segment.Group>
+    )
 }
