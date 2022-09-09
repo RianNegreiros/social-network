baad6e287f67f0eaac4ad0ae303c328115af4c3e
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-08 22:43:07 -0300

    refactor

--- client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,10 +7,11 @@ import LoadingComponent from "../../../layout/LoadingComponent";
 
 export default observer(function ActivityDashboard() {
   const {activityStore} = useStore();
+  const {loadActivities, activityRegistry} = activityStore;
 
   useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore])
+    if (activityRegistry.size <= 1) loadActivities();
+  }, [activityRegistry.size, loadActivities])
 
   if (activityStore.loadingInitial) return <LoadingComponent content="Loading app" />
   
