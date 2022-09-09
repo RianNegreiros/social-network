aa5aa478c02848cf03df070f5c564e0e0d60556f
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-09 19:52:46 -0300

    add register endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -45,5 +46,40 @@ namespace API.Controllers
 
         return Unauthorized();
     }
+
+    [HttpPost("register")]
+  public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+  {
+    if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
+    {
+        return BadRequest("Email taken");
+    }
+    if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
+    {
+        return BadRequest("Username taken");
+    }
+
+    var user = new AppUser
+    {
+        DisplayName = registerDto.DisplayName,
+        Email = registerDto.Email,
+        UserName = registerDto.Username
+    };
+
+    var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+    if (result.Succeeded)
+    {
+        return new UserDto
+        {
+            DisplayName = user.DisplayName,
+            Image = null,
+            Token = _tokenService.CreateToken(user),
+            Username = user.UserName
+        };
+    }
+
+    return BadRequest();
+  }
   }
 }
