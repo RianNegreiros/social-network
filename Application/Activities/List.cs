eb60cdd0451def587ccf4932cc1c3a836f042257
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-10 00:09:43 -0300

    load only related data

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,5 +1,5 @@
 using Application.Core;
-using Domain;
+using AutoMapper;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -8,18 +8,27 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>> {}
+        public class Query : IRequest<Result<List<ActivityDto>>> {}
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync(cancellationToken));
+                var activities = await _context.Activities
+                .Include(a => a.Attendees)
+                .ThenInclude(u => u.AppUser)
+                .ToListAsync(cancellationToken);
+
+                var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
+
+                return Result<List<ActivityDto>>.Success(activitiesToReturn);
             }
         }
     }
