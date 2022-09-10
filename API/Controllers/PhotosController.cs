82a9255ce737fd85f16d86609af9bb78cc1fa5ef
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-10 20:41:40 -0300

    add set main photo

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -16,5 +16,11 @@ namespace API.Controllers
         {
             return HandleResult(await Mediator.Send(new Delete.Command{Id = id}));
         }
+
+        [HttpPost("{id}/setMain")]
+        public async Task<IActionResult> SetMain(string id)
+        {
+            return HandleResult(await Mediator.Send(new SetMain.Command{Id = id}));
+        }
     }
 }
