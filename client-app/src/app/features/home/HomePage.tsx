a04bf6d629cb1b7d2664639b6491a2918b6f8cfc
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-09 21:41:56 -0300

    update home page and nav bar

--- client-app/src/app/features/home/HomePage.tsx
+++ client-app/src/app/features/home/HomePage.tsx
@@ -1,19 +1,30 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Header, Segment, Button } from 'semantic-ui-react';
+import { useStore } from '../../stores/store';
 
-export default function HomePage() {
+export default observer(function HomePage() {
+    const {userStore} = useStore();
     return (
         <Segment inverted textAlign='center' vertical className='masthead'>
             <Container text>
                 <Header as='h1' inverted>
                     Home Page
                 </Header>
-                <Header as='h2' inverted content='Welcome' />
-                <Button as={Link} to='/activities' size='huge' inverted>
-                    See Activities
-                </Button>
+                {userStore.isLogggedIn ? (
+                    <>
+                    <Header as='h2' inverted content='Welcome' />
+                    <Button as={Link} to='/activities' size='huge' inverted>
+                        See Activities
+                    </Button>
+                    </>
+                ) : (
+                    <Button as={Link} to='/login' size='huge' inverted>
+                        Login
+                    </Button>
+                )}
             </Container>
         </Segment>
     )
-}
+})
