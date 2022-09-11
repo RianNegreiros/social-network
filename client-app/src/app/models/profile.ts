79478b6a9a2f6e693bd68542db888acc06c746f3
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-10 21:33:52 -0300

    add profile store

--- client-app/src/app/models/profile.ts
+++ client-app/src/app/models/profile.ts
@@ -1,16 +1,23 @@
 import { User } from "./user";
 
 export interface Profile {
-  username: string;
-  displayName: string;
-  image?: string;
-  bio?: string;
+    username: string;
+    displayName: string;
+    image?: string;
+    bio?: string;
+    photos?: Photo[];
 }
 
 export class Profile implements Profile {
-  constructor(user: User) {
-    this.username = user.username;
-    this.displayName = user.displayName;
-    this.image = user.image;
-  }
+    constructor(user: User) {
+        this.username = user.username;
+        this.displayName = user.displayName;
+        this.image = user.image;
+    }
+}
+
+export interface Photo {
+    id: string;
+    url: string;
+    isMain: boolean;
 }
