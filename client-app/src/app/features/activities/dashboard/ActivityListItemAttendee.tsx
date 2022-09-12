4eed6d0beb5eb068d695b05a14239878b4d124be
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-12 01:30:52 -0300

    update ActivityListItemAttendee component image style

--- client-app/src/app/features/activities/dashboard/ActivityListItemAttendee.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityListItemAttendee.tsx
@@ -10,6 +10,10 @@ interface Props {
 }
 
 export default observer(function ActivityListItemAttendee({attendees}: Props) {
+  const styles = {
+    borderColor: 'orange',
+    borderWith: 2
+  }
   return (
     <List horizontal>
       {attendees.map(attendee => (
@@ -18,7 +22,13 @@ export default observer(function ActivityListItemAttendee({attendees}: Props) {
           key={attendee.username}
           trigger={
             <List.Item key={attendee.username} as={Link} to={`/profiles/${attendee.username}`}>
-              <Image size='mini' circular src={attendee.image || '/assets/user.png'} />
+              <Image 
+              size='mini' 
+              circular 
+              src={attendee.image || '/assets/user.png'} 
+              bordered
+              style={attendee.following ? styles : null}
+              />
             </List.Item>
           }
         >
