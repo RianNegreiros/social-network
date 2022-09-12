a63871830adfa748ac2830b3ccc881486f8cf0e2
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-12 20:13:32 -0300

    fix attendees property type

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,5 +1,3 @@
-using Application.Profiles;
-
 namespace Application.Activities
 {
   public class ActivityDto
@@ -13,6 +11,6 @@ namespace Application.Activities
         public string Venue { get; set; }
         public string HostUsername { get; set; }
         public bool IsCancelled { get; set; }
-        public ICollection<Profile> Attendees { get; set; }
+        public ICollection<AttendeeDto> Attendees { get; set; }
     }
 }
