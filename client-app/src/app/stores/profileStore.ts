707a21a505356c8449fdf9dc70415e0bb1052184
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-12 01:51:58 -0300

    add mobx reaction

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,4 +1,4 @@
-import { makeAutoObservable, runInAction } from "mobx";
+import { makeAutoObservable, reaction, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Photo, Profile } from "../models/profile";
 import { store } from "./store";
@@ -9,11 +9,27 @@ export default class ProfileStore {
     uploading = false;
     loading = false;
     followings: Profile[] = [];
-    loadingFollowings = false;
+    loadingFollowings: boolean = false;
     activeTab = 0;
 
     constructor() {
         makeAutoObservable(this);
+
+        reaction(
+            () => this.activeTab,
+            activeTab => {
+                if (activeTab === 3 || activeTab === 4) {
+                    const predicate = activeTab === 3 ? 'followers' : 'following';
+                    this.loadFollowings(predicate);
+                } else {
+                    this.followings = [];
+                }
+            }
+        )
+    }
+
+    setActiveTab = (activeTab: any) => {
+        this.activeTab = activeTab;
     }
 
     get isCurrentUser() {
