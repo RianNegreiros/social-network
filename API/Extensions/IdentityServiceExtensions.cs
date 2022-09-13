7d6782de4eb63cfeef9e234d4553afbd319a4ce7
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-13 20:14:02 -0300

    require confirmed email

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -10,16 +10,18 @@ using Persistence;
 
 namespace API.Extensions
 {
-  public static class IdentityServiceExtensions
+public static class IdentityServiceExtensions
     {
         public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddIdentityCore<AppUser>(opt =>
             {
                 opt.Password.RequireNonAlphanumeric = false;
+                opt.SignIn.RequireConfirmedEmail = true;
             })
             .AddEntityFrameworkStores<DataContext>()
-            .AddSignInManager<SignInManager<AppUser>>();
+            .AddSignInManager<SignInManager<AppUser>>()
+            .AddDefaultTokenProviders();
             
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
 
