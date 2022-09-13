64ca0b0a33e1de8d1e1593027dcf9f83fa1f9ff8
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-13 18:23:23 -0300

    update token validation params

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -30,7 +30,9 @@ namespace API.Extensions
                     ValidateIssuerSigningKey = true,
                     IssuerSigningKey = key,
                     ValidateIssuer = false,
-                    ValidateAudience = false
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
                 };
                 opt.Events = new JwtBearerEvents
                 {
