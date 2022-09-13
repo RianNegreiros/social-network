4d9c7e827fcbb0561e226aed7c1f937028480a20
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-13 20:18:15 -0300

    render menu items based on isLoggedIn state

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -5,26 +5,29 @@ import { Button, Container, Menu, Image, Dropdown } from 'semantic-ui-react';
 import { useStore } from "../stores/store";
 
 export default observer(function NavBar() {
-  const {userStore: {user, logout}} = useStore();
+  const {userStore: {user, logout, isLogggedIn}} = useStore();
   return (
     <Menu inverted fixed="top">
       <Container>
         <Menu.Item as={NavLink} to='/' exact header>
           Home
         </Menu.Item>
-        <Menu.Item as={NavLink} to='/activities' name="Activivities" />
-        <Menu.Item>
-          <Button as={NavLink} to='/createActivity' positive content="Create Activity" />
-        </Menu.Item>
-        <Menu.Item position="right">
-          <Image src={user?.image || '/assets/user.png'} avatar spaced='right' />
-          <Dropdown pointing='top left' text={user?.displayName}>
-            <Dropdown.Menu>
-            <Dropdown.Item as={Link} to={`/profiles/${user?.username}`} text="My profile" icon='user' />
-            <Dropdown.Item onClick={logout} text='Logout' icon='power' />
-            </Dropdown.Menu>
-          </Dropdown>
-        </Menu.Item>
+        {isLogggedIn &&
+        <>
+          <Menu.Item as={NavLink} to='/activities' name="Activivities" />
+          <Menu.Item>
+            <Button as={NavLink} to='/createActivity' positive content="Create Activity" />
+          </Menu.Item>
+          <Menu.Item position="right">
+            <Image src={user?.image || '/assets/user.png'} avatar spaced='right' />
+            <Dropdown pointing='top left' text={user?.displayName}>
+              <Dropdown.Menu>
+              <Dropdown.Item as={Link} to={`/profiles/${user?.username}`} text="My profile" icon='user' />
+              <Dropdown.Item onClick={logout} text='Logout' icon='power' />
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Item>
+        </>}
       </Container>
     </Menu>
   )
