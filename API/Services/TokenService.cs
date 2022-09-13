bc4e3d92ed535aefc9dba7a234be4f7b1b1c1776
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-13 18:32:31 -0300

    change token expires time

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -29,7 +29,7 @@ namespace API.Services
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.UtcNow.AddMinutes(1),
+                Expires = DateTime.UtcNow.AddMinutes(10),
                 SigningCredentials = creds
             };
 
